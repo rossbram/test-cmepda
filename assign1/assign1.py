a25b1930dec77326b4ac52c09f8ba6b68f5086f4
Ross Bramato <rossellabramato@gmail.com>
2025-09-30 15:51:24 +0200

    Update assign1.py
    Added comments and a print character count option.

--- assign1/assign1.py
+++ assign1/assign1.py
@@ -6,7 +6,7 @@ import textwrap
 # make the frequencies prettier and add each letter
 # the program should have an option to display a histogram of the frequences
 # [optional] the program should have an option to skip the parts of the text
-#  that do not pertain to the book (e.g., preamble and license)
+#            that do not pertain to the book (e.g., preamble and license)
 # think of more book stats
 
 parser = argparse.ArgumentParser(
@@ -37,24 +37,25 @@ parser.add_argument('--totl', action='store_true',  help='displays the total num
 args = parser.parse_args()
 fname = args.filename
 
-EachCount = [0] * 26 ## Altrimenti, importando numpy, np.zeroes(26)
-letters = 'abcdefghijklmnopqrstuvwxyz'
-lcount = 0
+EachCount = [0] * 26 ## could be np.zeroes(26) if importing numpy, but seems superfluous
+letters = 'abcdefghijklmnopqrstuvwxyz' ## sstring that will be cycled though with the l variable later
+lcount = 0 ## contains the total number of each letter which is counted in the cycle later
 
-a = {'a','à','á','â'}
+a = {'a','à','á','â'} ## yay for accented letters!
 e = {'e','è','é','ê'}
 i = {'i','î','ì','í'}
 o = {'o','ô','ò','ó'}
 u = {'u','û','ú','ù'}
 
-start = time.time()
+start = time.time() ## stores the starting time
 try:
-    with open(fname, 'r', encoding='utf-8') as fhandle:
-        text = fhandle.read().lower()
+    with open(fname, 'r', encoding='utf-8') as fhandle: ## without the encoding, the dialogue quotes break everything
+        text = fhandle.read().lower() ## reads the file as one single piece, and lowercase
         for l in letters:
             lcount = 0
-            if l == 'a':
-                lcount = sum(1 for ch in text if ch in a)
+            if l == 'a': ## there's probably a cleaner way to do this but i can't quite think of it rn
+                lcount = sum(1 for ch in text if ch in a) ## adds 1 to lcount for each character instance in the text that corresponds
+                                                          ## to one of the characters in the list a (similarly for the other vowels)
             elif l == 'e':
                 lcount = sum(1 for ch in text if ch in e)
             elif l == 'i':
@@ -65,30 +66,39 @@ try:
                 lcount = sum(1 for ch in text if ch in u)
             else:
                 lcount = sum(1 for ch in text if ch == l)
-            EachCount[letters.find(l)] = lcount
-        TotCount = sum(EachCount)
-        EachFrequence = [x * 100 / TotCount for x in EachCount]
-        EFNice = [f'{x:.2f}%' for x in EachFrequence]
-        end = time.time() - start
+            EachCount[letters.find(l)] = lcount ## each element in EachCount corresponds to a letter of the alphabet string
+                                                ## this line adds the number of instances to the corresponding position in the array
+        TotCount = sum(EachCount)   ## sums all the instances, so contains the total no. of characters excluding punctuaction&spaces
+        EachFrequence = [x * 100 / TotCount for x in EachCount] ## again there's a correspondence w the alphabet
+                                                                ##but now there's the frequency, not the plain number
+        EFNice = [f'{x:.2f}%' for x in EachFrequence]   ## default formatting has tons of ugly decimals, this makes it cleaner
         print(*EFNice, sep=', ')
         
-    with open(fname, 'r', encoding='utf-8') as fhandle:
+    with open(fname, 'r', encoding='utf-8') as fhandle: ## the if doesn't work without specifying the with ... again
         if args.totl:
-            TotLines = 0
+            TotLines = 0 ## initializing the line count to 0
             for line in fhandle:
-                NonEmptyLine = line.strip()
+                NonEmptyLine = line.strip() ## removes whitespaces, so the following if skips empty lines
                 if NonEmptyLine: 
                     TotLines+=1
             print('The total number of lines is: ', TotLines)
 
     with open(fname, 'r', encoding='utf-8') as fhandle:
         if args.totw:
-            TotWords = 0
+            TotWords = 0 ## initializing the word count to 0
             for line in fhandle:
-                ThisLineWords = len(line.split())
+                ThisLineWords = len(line.strip().split()) ## split generates an array with each word as an element
+                                                          ## so its len is the no of words in the line
+                                                          ## not sure if the .strip() is necessary but better safe than sorry
                 TotWords += ThisLineWords
             print('The total number of words is: ', TotWords)
-    
+
+    with open(fname, 'r', encoding='utf-8') as fhandle:
+        if args.totc:
+            print('The total number of characters is: ', TotCount)
+
+    end = time.time() - start ## compares w the starting time    
     print('The elapsed time is: ', f'{end:.2f}', 'seconds')
+
 except FileNotFoundError:
     print('Could not find', fname, '\b.')    
